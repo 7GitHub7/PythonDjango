df884505332bc02894d751207b2b82f8fc4807d3
Piotrek <amrozp@gmail.com>
2021-01-30 18:47:58 +0100

    init project

---
+++ django_project/blog/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+posts = [
+    {
+        'author':'Tadek',
+        'title' :'Tech',
+        'content':'First post',
+        'date_posted':'August 27, 2020'
+    },
+    {
+        'author':'Janusz',
+        'title' :'Diet',
+        'content':'Second post',
+        'date_posted':'August 17, 2020'
+    }
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html',{'title':'About'})
