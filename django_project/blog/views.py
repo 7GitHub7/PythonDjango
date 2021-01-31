d8bd088f812fd18817992cfc0e4f10e832c8f84e
Piotrek <amrozp@gmail.com>
2021-01-31 13:55:45 +0100

    added sqlite

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author':'Tadek',
-        'title' :'Tech',
-        'content':'First post',
-        'date_posted':'August 27, 2020'
-    },
-    {
-        'author':'Janusz',
-        'title' :'Diet',
-        'content':'Second post',
-        'date_posted':'August 17, 2020'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
 
     return render(request, 'blog/home.html', context)
