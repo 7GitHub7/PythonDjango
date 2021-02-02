bbdf60e9ee29bda2d831a25fab2625006d2114b6
Piotrek <amrozp@gmail.com>
2021-02-02 22:39:10 +0100

    added detailed post view

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, DetailView
 
 @login_required
 def home(request):
@@ -10,5 +11,17 @@ def home(request):
 
     return render(request, 'blog/home.html', context)
 
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+    
+        
 def about(request):
-    return render(request, 'blog/about.html',{'title':'About'})
+    return render(request, 'blog/about.html',{'title':'About'}),
+
